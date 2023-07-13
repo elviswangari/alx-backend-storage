ff3c293f8cc8f4428d778a854f2f4267cb672db5
elviswangari <elviswangare@gmail.com>
2023-07-13 12:16:28 +0300

    create table users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,3 @@
+-- script to create a table
+-- must not fail if exists
+CREATE TABLE IF NOT EXISTS `users` (`id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(255) NOT NULL UNIQUE, `name` VARCHAR(255), PRIMARY KEY(`id`));
