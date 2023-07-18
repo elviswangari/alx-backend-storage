525eeb5fed2b7ea7c94419110e121cbbbd9abff0
elviswangari <elviswangare@gmail.com>
2023-07-18 20:43:57 +0300

    update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+change topic in a collection
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''
+    change topics
+    '''
+    mongo_collection.update_many(
+            {'name': name},
+            {'$set': {'topics': topics}}
+            )
