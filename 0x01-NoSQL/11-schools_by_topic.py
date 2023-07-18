3f841faed89185e1ae9e9a5dfadae14f9c45e8fe
elviswangari <elviswangare@gmail.com>
2023-07-18 20:57:00 +0300

    list and topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,5 +8,5 @@ def schools_by_topic(mongo_collection, topic):
     '''
     return a list
     '''
-    topics = {'topic': {'$eq': topic}}
+    topics = {'topics': {'$eq': topic}}
     return [item for item in mongo_collection.find(topics)]
