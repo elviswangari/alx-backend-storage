c5fcdf2e228a2f81eaefbee206555b7fe6b6a588
elviswangari <elviswangare@gmail.com>
2023-07-19 16:56:26 +0300

    implement new methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''
 basics of redis
 '''
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 import redis
 
@@ -23,6 +23,34 @@ class Cache:
         '''
         generates random key
         '''
-        key = str(uuid4())
-        self._redis.set(key, data)
-        return key
+        keys = str(uuid4())
+        self._redis.set(keys, data)
+        return keys
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''
+        convert to desired format
+        '''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''
+        parametize Cache.get with correct format
+        '''
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        '''
+        parametize Cache.get with correct format
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
